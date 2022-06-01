b478a2fdffaf1863b7f5f072d132324f90ae6062
Luke Yost <lukexyost@gmail.com>
2022-06-01 16:18:35 -0600

    made model and a bit of controller

---
+++ app/Controllers/PokeAPIPokemons.js
@@ -0,0 +1,9 @@
+import { ProxyState } from "../AppState.js";
+import { Pokemon } from "../Models/Pokemon.js";
+
+
+export class PokeAPIPokemonsController{
+  constructor(){
+    console.log('load the mons');
+  }
+}
